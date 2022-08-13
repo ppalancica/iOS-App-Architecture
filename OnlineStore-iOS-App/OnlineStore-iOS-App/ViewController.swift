da374dc592f4772f7c31643dfd0ea7844cd8995b
System Administrator <root@Pavels-MacBook-Pro.local>
2022-08-14 01:30:59 +0300

    CreatediOS App project in Xcode

---
+++ OnlineStore-iOS-App/OnlineStore-iOS-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineStore-iOS-App
+//
+//  Created by Pavel Palancica on 8/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OnlineStore-iOS-App/OnlineStore-iOS-AppUITests/OnlineStore_iOS_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  OnlineStore_iOS_AppUITestsLaunchTests.swift
+//  OnlineStore-iOS-AppUITests
+//
+//  Created by Pavel Palancica on 8/14/22.
+//
+
+import XCTest
+
+class OnlineStore_iOS_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
